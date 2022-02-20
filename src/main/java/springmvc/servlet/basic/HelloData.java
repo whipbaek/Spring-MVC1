a4e4c1bddec9c0cab9789c5d409a82f1fe0b05ef
whipbaek <jibaek0513@gmail.com>
2022-02-20 21:13:38 +0900

    spring servlet2

---
+++ src/main/java/springmvc/servlet/basic/HelloData.java
@@ -0,0 +1,13 @@
+package springmvc.servlet.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HelloData {
+
+    private String username;
+    private int age;
+
+}
